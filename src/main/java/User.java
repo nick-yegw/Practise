a5290d2f6a035fbd3567061f2cb5ce01ae5a7387
nickel-ye <weijing3.28@gmail.com>
2013-02-26 23:49:38 +0800

    Add the project to the Git

---
+++ src/main/java/User.java
@@ -0,0 +1,64 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nickle
+ * Date: 13-2-26
+ * Time: 下午11:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class User {
+
+    private Integer userId;
+    private String account;
+    private String password;
+    private String name;
+    private String phone;
+    private String address;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
